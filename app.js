00c7e5f94b9dd90e5e61b58e60f9015b59b95901
MariaAddict <mashakan27@gmail.com>
2020-11-13 18:17:22 +0700

    feat: добавить переход на /users, отправить данные user.json, настроить поиск по id юзера

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
+const UsersRouter = require('./routes/users.js');
 
+app.use('/', UsersRouter);
+app.use('*', (req, res) => {
+  res.status(404).send({ "message": "Запрашиваемый ресурс не найден" });
+});
 
 app.listen(PORT, () => {
   console.log(`listening port:  ${PORT}`);
---
+++ routes/users.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs').promises;
+
+router.get('/users', (req, res) => {
+  const filePath = path.join(__dirname, '..', 'data', 'users.json');
+  const fileReader = fs.createReadStream(filePath, { encoding: 'utf8' });
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  fileReader.pipe(res);
+});
+
+router.get('/users/:id', (req, res) => {
+  const filePath = path.join(__dirname, '..', 'data', 'users.json');
+  const fileReader = fs.createReadStream(filePath, { encoding: 'utf8' });
+
+  const {id} = req.params;
+
+fsPromises.readFile(filePath, { encoding: 'utf8' })
+    .then((data) => {return JSON.parse(data)})
+    .then((data) => {
+      const user = data.find((item) => {;
+        return item._id === id;
+      });
+        res.send(user);
+
+        if (!user) {
+          res.status(404).send({ "message": "Нет пользователя с таким id" });
+        }
+
+    })
+    .catch(err => {
+        res.status(404).send({ "message": "Нет такого файла" });
+    });
+});
+
+module.exports = router;
