efa05838721d8bab10fd65d77885ad2161a1df63
MariaAddict <mashakan27@gmail.com>
2020-12-03 00:28:22 +0700

    feat: установить mongoose, подключить mongoose в api.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const path = require('path');
 
 const app = express();
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
