d1be5b178cf5de5ed3d1bfae02abceb07d2ee9d6
MariaAddict <mashakan27@gmail.com>
2020-12-04 19:41:33 +0700

    feat: добавить обновление данных пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -30,8 +30,24 @@ const createUser = (req, res) => User.create({ ...req.body })
     if (err.name === 'ErrorName') return res.status(400).send({ message: 'Некорректные данные пользователя' });
   });
 
+// eslint-disable-next-line no-unused-vars
+const updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  return User.findByIdAndUpdate(req.user._id, { name, about },
+    {
+      new: true,
+      upsert: true,
+    })
+    .then((data) => res.send(data))
+    // eslint-disable-next-line consistent-return
+    .catch((err) => {
+      if (err.name === 'ErrorName') return res.status(400).send({ message: 'Данные пользователя не обновились' });
+    });
+};
+
 module.exports = {
   getUsers,
   getUser,
   createUser,
+  updateProfile,
 };
