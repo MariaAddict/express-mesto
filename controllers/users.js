516e770b7437945ce0864fd8bb9293a8c8e974c0
MariaAddict <mashakan27@gmail.com>
2020-12-04 19:47:15 +0700

    feat: добавить обновление аватара пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -45,9 +45,24 @@ const updateProfile = (req, res) => {
     });
 };
 
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  return User.findByIdAndUpdate(req.user._id, { avatar },
+    {
+      new: true,
+      upsert: true,
+    })
+    .then((data) => res.send(data))
+    // eslint-disable-next-line consistent-return
+    .catch((err) => {
+      if (err.name === 'ErrorName') return res.status(400).send({ message: 'Данные пользователя не обновились' });
+    });
+};
+
 module.exports = {
   getUsers,
   getUser,
   createUser,
   updateProfile,
+  updateAvatar,
 };
