44cc0ecec0c7ee449e33a77e5431d4d645ba14f8
MariaAddict <mashakan27@gmail.com>
2020-12-06 19:04:26 +0700

    style: убрать регистр в названии роутеров в app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 
-const UsersRouter = require('./routes/users.js');
-const CardsRouter = require('./routes/cards.js');
+const usersRouter = require('./routes/users.js');
+const cardsRouter = require('./routes/cards.js');
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -24,8 +24,8 @@ app.use((req, res, next) => {
 
   next();
 });
-app.use('/', UsersRouter);
-app.use('/', CardsRouter);
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
